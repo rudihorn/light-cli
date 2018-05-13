807a504e68ae93065686f9306fc4d7154c8d1ac8
Rudi Horn <dyn-git@rudi-horn.de>
2018-05-13 13:27:20 +0100

    set up travis for multiple targets

--- ci/install.sh
+++ ci/install.sh
@@ -1,6 +1,10 @@
 set -euxo pipefail
 
 main() {
+    if [ "$TARGET" = "thumbv7m-none-eabi" ]; then
+	    rustup install target thumbv7m-none-eabi
+    fi
+
     return
 }
 
--- ci/script.sh
+++ ci/script.sh
@@ -2,6 +2,11 @@ set -euxo pipefail
 
 main() {
     cargo check --target $TARGET
+
+    if [ "$TARGET" = "thumbv7m-none-eabi" ]; then
+        cargo check --example stm32 --target $TARGET
+    fi
+
     cargo test --target $TARGET
 }
 
